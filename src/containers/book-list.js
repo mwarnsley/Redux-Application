042ffd310dd851b2dc238f0e40644c5326ef9830
Marcus Warnsley <mwarnsley@Marcuss-MacBook-Pro.local>
2016-05-18 23:13:33 -0400

    Adding more files and fixing the book-list js file

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookList extends Component{
+class BookList extends Component{
     renderList(){
-        return this.props.books.map(book) => {
+        return this.props.books.map((book) => {
             return(
                 <li key={ book.title } className="list-group-item">
                     { book.title }
                 </li>
             );
-        }    
+        });  
     }
     
     render(){
@@ -19,6 +20,15 @@ export default class BookList extends Component{
         );
     }
 }
+
+//Whatever is returned from this will show up as props inside of BookList
+function mapStateToProps(state){
+    return{
+      books: state.books  
+    };
+}
+
+export default connect(mapStateToProps)(BookList);
 
 
 
